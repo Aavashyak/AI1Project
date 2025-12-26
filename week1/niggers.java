83462354b69832acf7b4a2f964d061856780c26e
Aavashyak <pokharelanju77@gmail.com>
2025-12-26 21:20:15 +0545

    week 7 and 8 update

---
+++ week1/niggers.java
@@ -0,0 +1,12 @@
+package week1;
+import java.util.Scanner;
+public class niggers
+{
+    public static void main (String args[])
+    {
+        Scanner ni = new Scanner(System.in);
+        System.out.println("Who are the worst people in this world");
+        ni.nextLine();
+        System.out.println("Good Job");
+    }
+}
